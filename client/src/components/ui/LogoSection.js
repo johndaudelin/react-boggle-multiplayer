962348d525d45be4ac6792c760805aceeccbd15c
John Daudelin <johndaudelin46@gmail.com>
2020-09-15 13:07:37 -0400

    Add simple hit counter

--- client/src/components/ui/LogoSection.js
+++ client/src/components/ui/LogoSection.js
@@ -3,6 +3,14 @@ import '../../stylesheets/LogoSection.scss'
 import logo from '../../img/merriam-webster.png' // Tell webpack this JS file uses this image
 
 export default class LogoSection extends React.Component {
+  componentDidMount () {
+    const script = document.createElement('script')
+
+    script.src = 'https://www.powr.io/powr.js?platform=react'
+    script.async = true
+
+    document.body.appendChild(script)
+  }
   render () {
     return (
       <div className='centeredColumn'>
@@ -10,6 +18,7 @@ export default class LogoSection extends React.Component {
         <a href='https://dictionaryapi.com/' target='_blank'>
           <img src={logo} alt='Logo' className='logo' />
         </a>
+        <div class='powr-hit-counter' id='094d5e9f_1600189066'></div>
       </div>
     )
   }
