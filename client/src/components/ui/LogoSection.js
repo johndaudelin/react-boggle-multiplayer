d25d535a22a981c71055e320368e53f454773b33
John Daudelin <johndaudelin46@gmail.com>
2021-10-04 20:28:39 -0400

    Remove ugly hit counter

--- client/src/components/ui/LogoSection.js
+++ client/src/components/ui/LogoSection.js
@@ -3,7 +3,7 @@ import '../../stylesheets/LogoSection.scss'
 import logo from '../../img/merriam-webster.png' // Tell webpack this JS file uses this image
 
 export default class LogoSection extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     const script = document.createElement('script')
 
     script.src = 'https://www.powr.io/powr.js?platform=react'
@@ -11,7 +11,7 @@ export default class LogoSection extends React.Component {
 
     document.body.appendChild(script)
   }
-  render () {
+  render() {
     return (
       <div className='logoSection'>
         <span
@@ -26,7 +26,6 @@ export default class LogoSection extends React.Component {
         <a href='https://dictionaryapi.com/' target='_blank'>
           <img src={logo} alt='Logo' className='logo' />
         </a>
-        <div className='powr-hit-counter' id='094d5e9f_1600189066'></div>
       </div>
     )
   }
