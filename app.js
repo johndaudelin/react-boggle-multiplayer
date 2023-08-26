64c509f958f958b26eb1f74e637a91aef5d346fe
John Daudelin <johndaudelin46@gmail.com>
2023-08-26 00:16:32 -0400

    Add /health route

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-const express = require('express')
-const http = require('http')
-const path = require('path')
-const socketIo = require('socket.io')
-const c = require('./constants.js')
-const actions = require('./actions.js')
-const { getConfig } = require('./helpers')
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIo = require("socket.io");
+const c = require("./constants.js");
+const actions = require("./actions.js");
+const { getConfig } = require("./helpers");
 
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4001;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'client/dist')))
+app.use(express.static(path.join(__dirname, "client/dist")));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
-})
+app.get("/health", function (req, res) {
+  console.log("Received health check request");
+  res.sendStatus(200);
+});
 
-const server = http.createServer(app)
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
-const io = socketIo(server)
+const server = http.createServer(app);
+
+const io = socketIo(server);
 const gameState = {
-  rooms: []
-}
-const intervals = {}
+  rooms: [],
+};
+const intervals = {};
 
-exports.io = io
-exports.gameState = gameState
-exports.intervals = intervals
+exports.io = io;
+exports.gameState = gameState;
+exports.intervals = intervals;
 
-io.on('connection', socket => {
-  console.log('New client connected')
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
   socket.on(c.SOCKET_EVENTS.LEAVE_ROOM, () => {
-    const config = getConfig(socket)
-    actions.leaveRoom(socket, config)
-  })
-  socket.on(c.SOCKET_EVENTS.ENTER_ROOM, config =>
+    const config = getConfig(socket);
+    actions.leaveRoom(socket, config);
+  });
+  socket.on(c.SOCKET_EVENTS.ENTER_ROOM, (config) =>
     actions.enterRoom(socket, config)
-  )
+  );
   socket.on(c.SOCKET_EVENTS.START_GAME, () => {
-    const config = getConfig(socket)
-    actions.startGame(socket, config)
-  })
-  socket.on(c.SOCKET_EVENTS.ADD_SCORECARD, scorecard => {
-    const config = getConfig(socket)
-    actions.addScorecard(socket, config, scorecard)
-  })
+    const config = getConfig(socket);
+    actions.startGame(socket, config);
+  });
+  socket.on(c.SOCKET_EVENTS.ADD_SCORECARD, (scorecard) => {
+    const config = getConfig(socket);
+    actions.addScorecard(socket, config, scorecard);
+  });
 
-  socket.on('disconnect', () => {
-    const config = getConfig(socket)
+  socket.on("disconnect", () => {
+    const config = getConfig(socket);
     if (config.roomName) {
-      actions.leaveRoom(socket, config)
+      actions.leaveRoom(socket, config);
     }
-    console.log('Client disconnected')
-  })
-})
+    console.log("Client disconnected");
+  });
+});
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.log(`Listening on port ${port}`));
